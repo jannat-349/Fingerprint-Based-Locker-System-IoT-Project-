2ad565e5ed856c6c7ae82850d23ded019cd79d18
jannat-349 <jucse28.349@gmail.com>
2024-05-02 18:16:23 +0600

    Added the templates of main page and payment page

---
+++ app/src/main/java/com/example/taplock/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.taplock;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnEnter = findViewById(R.id.btn_enter);
+        btnEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to another activity or XML layout here
+                startActivity(new Intent(MainActivity.this, PaymentActivity.class));
+            }
+        });
+    }
+}
